6760e84938956601aed53ed40281be7c5e8af816
VeraMalakovich <kalipso.v@gmail.com>
2023-06-20 22:40:37 +0300

    2.7 Selected helper classes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,15 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
-import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-    app.goToGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroups();
-    app.returnToGroupPage();
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
   }
 }
