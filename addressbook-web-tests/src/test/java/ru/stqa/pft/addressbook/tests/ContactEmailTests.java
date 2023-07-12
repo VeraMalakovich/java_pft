19f2c1d28cbc8484877d625224318876cd44ca8c
VeraMalakovich <kalipso.v@gmail.com>
2023-07-12 14:28:01 +0300

    Hometask 11.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
@@ -9,38 +9,38 @@ import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTest extends TestBase{
+public class ContactEmailTests extends TestBase{
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().homePage();
     if (app.contact().all().size() == 0) {
       app.contact().create(new ContactData().withFirstName("Vera").withMiddleName("Anatolevna").withLastName("Malakovich")
-              .withNickName("nickName").withTitle("Title").withCompany("companyName").withAddress("addressName")
+              .withNickName("nickName").withTitle("Title").withCompany("companyName").withAddress("Ptituchkogo 86, 30")
               .withHomePhone("315-38-12").withMobilePhone("+375295789988").withWorkPhone("22 333 444").withFaxPhone("faxPhone")
-              .withEmail("email1").withNewGroup("test1").withHomePage("homePage"));
+              .withEmail("email1").withEmail2("vera2@tut.by").withEmail3("vera3@tut.by").withNewGroup("test1")
+              .withSecondAddress("Minsk").withSecondHomePhone("+375295789528").withHomePage("homePage"));
     }
   }
 
   @Test
-  public void testContactPhones() {
+  public void testContactEmails() {
     app.goTo().homePage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
   }
 
-  private String mergePhones(ContactData contact) {
-    return Stream.of(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getSecondHomePhone())
+  private String mergeEmails(ContactData contact) {
+    return Stream.of(contact.getEmail(), contact.getSecondEmail(), contact.getThirdEmail())
             .filter((s) -> !s.equals(""))
-            .map(ContactPhoneTest::cleaned)
+            .map(ContactEmailTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
   public static String cleaned(String phone) {
-    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+    return phone.replaceAll("\\s", "");
   }
 }
