2d5f91257709768c810c2143637432b338df4a52
VeraMalakovich <kalipso.v@gmail.com>
2023-07-18 18:21:44 +0300

    6.10 Description of the test stand configuration

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,16 +5,26 @@ import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
 
+import java.io.IOException;
+
 public class TestBase {
-  protected static final ApplicationManager app = new ApplicationManager(Browser.FIREFOX.browserName());
+  protected static final ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", Browser.FIREFOX.browserName()));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @BeforeSuite(alwaysRun = true)
-  public void setUp() {
+  public void setUp() throws IOException {
     app.init();
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
     app.stop();
   }
 }
